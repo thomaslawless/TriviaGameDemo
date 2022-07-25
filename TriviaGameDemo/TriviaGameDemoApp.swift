0d637ff8b47a415f79e76f6aaaa23885b9f1e1a9
Thomas Lawless <55810024+thomaslawless@users.noreply.github.com>
2022-07-25 13:12:58 -0400

    Initial Commit

---
+++ TriviaGameDemo/TriviaGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameDemoApp.swift
+//  TriviaGameDemo
+//
+//  Created by Thomas Lawless III on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
